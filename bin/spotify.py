1c43b8ff900d5e410b0afa0ac68b5e9104d220bf
tretinha <gustavo@tretinha.com>
2020-07-09 15:30:55 -0300

    Update dbus object: spotify -> spotifyd

--- bin/spotify.py
+++ bin/spotify.py
@@ -7,11 +7,11 @@ import sys
 
 try:
     bus = dbus.SessionBus()
-    spotify = bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+    spotifyd = bus.get_object("org.mpris.MediaPlayer2.spotifyd", "/org/mpris/MediaPlayer2")
 
 
     if os.environ.get('BLOCK_BUTTON'):
-        control_iface = dbus.Interface(spotify, 'org.mpris.MediaPlayer2.Player')
+        control_iface = dbus.Interface(spotifyd, 'org.mpris.MediaPlayer2.Player')
         if (os.environ['BLOCK_BUTTON'] == '1'):
             control_iface.Previous()
         elif (os.environ['BLOCK_BUTTON'] == '2'):
@@ -19,8 +19,8 @@ try:
         elif (os.environ['BLOCK_BUTTON'] == '3'):
             control_iface.Next()
 
-    spotify_iface = dbus.Interface(spotify, 'org.freedesktop.DBus.Properties')
-    props = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+    spotifyd_iface = dbus.Interface(spotifyd, 'org.freedesktop.DBus.Properties')
+    props = spotifyd_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
 
     if (sys.version_info > (3, 0)):
         print(str(props['xesam:artist'][0]) + " - " + str(props['xesam:title'].partition('-')[0]))
