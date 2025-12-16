03d38bc3b256e9c6035862e60f448605c5c1288e
colombini-gw <gustavo.colombini@glydways.com>
2025-12-16 16:27:57 -0300

    Auto-commit: 2025-12-16 16:27:57

--- nix-config/hosts/work/home.nix
+++ nix-config/hosts/work/home.nix
@@ -217,6 +217,17 @@ in
     "Documents/Invoices/secrets.json" = {
       source = ../../config/scripts/secrets.json;
     };
+
+    # Openbox session file for display manager
+    ".local/share/xsessions/openbox.desktop".text = ''
+      [Desktop Entry]
+      Name=Openbox
+      Comment=Log in using the Openbox window manager
+      Exec=${config.home.profileDirectory}/bin/openbox-session
+      TryExec=${config.home.profileDirectory}/bin/openbox
+      Type=Application
+      DesktopNames=Openbox
+    '';
   };
 
   # Systemd user services
