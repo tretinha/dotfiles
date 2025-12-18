04279afd08f18c9dc06aa213810ba3cc3f27b6e8
colombini-gw <gustavo.colombini@glydways.com>
2025-12-18 09:00:30 -0300

    Auto-commit: 2025-12-18 09:00:30

--- nix-config/config/nvim/lua/plugins/nvim-treesitter.lua
+++ nix-config/config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,13 @@
 return {
+  { "tpope/vim-sleuth" }, -- detects indentation of the file for tabs, helping treesitter's indent
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    -- Optional: Configure Treesitter modules and parsers
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = { "c", "lua", "vimdoc", "cue", "terraform", "yaml", "gotmpl", "helm" }, -- Specify parsers to install
-        highlight = { enable = false }, -- Enable highlighting
-        indent = { enable = false }, -- Enable indentation
-        -- Add other modules as needed
+        highlight = { enable = false }, -- Not particularly a fan
+        indent = { enable = true },
       })
     end,
   },
