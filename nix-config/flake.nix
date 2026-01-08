80f93ba89db0403a20c28dbed44263fb132633ca
colombini-gw <gustavo.colombini@glydways.com>
2026-01-08 17:00:52 -0300

    Auto-commit: 2026-01-08 17:00:52

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -40,6 +40,10 @@
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
     };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
