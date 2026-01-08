91942cbfd3e13b8b20d45e50b4a62c9414642798
colombini-gw <gustavo.colombini@glydways.com>
2026-01-08 11:00:02 -0300

    Auto-commit: 2026-01-08 11:00:02

--- nix-config/config/nvim/lua/plugins/fzf.lua
+++ nix-config/config/nvim/lua/plugins/fzf.lua
@@ -16,7 +16,8 @@ return {
       -- fzf_colors = false, 
     },
     keys = {
-      { "<leader>ff", function() require("fzf-lua").git_files() end, desc = "Find Files" },
+      { "<leader>ff", function() require("fzf-lua").files() end, desc = "Find Files" },
+      { "<leader>fg", function() require("fzf-lua").git_files() end, desc = "Find Files" },
       { "<leader>fb", function() require("fzf-lua").buffers() end, desc = "Find Buffers" },
       { "<leader>fr", function() require("fzf-lua").grep() end, desc = "Grep" },
       { "<leader>fd", function() require("fzf-lua").git_files({cwd=vim.fn.expand("%:p:h")}) end, desc = "Find files in cwd" },
