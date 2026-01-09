4871e0f902f37ab0495c88adfabd4b24bbe54494
colombini-gw <gustavo.colombini@glydways.com>
2026-01-09 09:21:22 -0300

    Auto-commit: 2026-01-09 09:21:22

--- nix-config/hosts/work/niri.nix
+++ nix-config/hosts/work/niri.nix
@@ -2,144 +2,21 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+    nix_config_path = "${config.home.homeDirectory}/dotfiles/nix-config";
+    nix_xdg_config = "${nix_config_path}/config";
+    create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  in {
+
+  xdg.configFile = {
+    "waybar" = {
+      source = create_symlink "${nix_xdg_config}/waybar";
+      recursive = true;
+    };
+  };
+
   programs.waybar = {
     enable = true;
-    settings = {
-      main = {
-        layer = "top";
-        position = "top";
-        height = 26;
-
-        modules-left = [
-          "disk"
-          "temperature"
-          "memory"
-          "cpu"
-        ];
-
-        modules-center = [
-          "niri/workspaces"
-        ];
-
-        modules-right = [
-          "tray"
-          "network"
-          "wireplumber"
-          "battery"
-          "clock"
-        ];
-
-        battery = {
-          interval = 60;
-          states = {
-            warning = 30;
-            critical = 15;
-          };
-          format = "{icon}  {capacity}%";
-          format-icons = [
-            ""
-            ""
-            ""
-            ""
-            ""
-          ];
-          max-length = 25;
-        };
-
-        clock = {
-          format = " {:%A, %d %b %Y %H:%M}";
-          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-          calendar-weeks-pos = "right";
-          today-format = "<span color='#ff6699'><b><u>{}</u></b></span>";
-          format-calendar = "<span color='#ecc6d9'><b>{}</b></span>";
-          format-calendar-weeks = "<span color='#99ffdd'><b>W{:%V}</b></span>";
-          format-calendar-weekdays = "<span color='#ffcc66'><b>{}</b></span>";
-          on-scroll = {
-            calendar = 1;
-          };
-        };
-
-        cpu = {
-          interval = 5;
-          format = " {usage}%";
-          states = {
-            warning = 70;
-            critical = 90;
-          };
-        };
-
-        disk = {
-          interval = 30;
-          format = " {percentage_used}%";
-          path = "/";
-        };
-
-        memory = {
-          interval = 5;
-          format = " {}%";
-          states = {
-            warning = 70;
-            critical = 90;
-          };
-        };
-
-        network = {
-          format = "";
-          format-ethernet = "󰈀 ";
-          format-wifi = "{icon}";
-          format-disconnected = "󰲛 ";
-          format-icons = [
-            "󰤯 "
-            "󰤟 "
-            "󰤢 "
-            "󰤥 "
-            "󰤨 "
-          ];
-          tooltip-format-wifi = "{essid}({signalStrength}%) {ipaddr}";
-          tooltip-format-ethernet = "{ifname} {ipaddr}";
-          tooltip-format-disconnected = "Disconnected";
-          on-click = "nm-connection-editor";
-        };
-
-        "niri/workspaces" = {
-          format = "{icon}";
-          on-click = "activate";
-          format-icons = {
-            "active" = "";
-            "default" = "";
-          };
-          icon-size = 10;
-        };
-
-        temperature = {
-          hwmon-path = "/sys/class/hwmon/hwmon2/temp1_input";
-          critical-threshold = 80;
-          format-critical = " {temperatureC}°C";
-          format = " {temperatureC}°C";
-          interval = 2;
-        };
-
-        tray = {
-          icon-size = 16;
-          spacing = 16;
-        };
-
-        wireplumber = {
-          "format" = "  {volume}%";
-          "max-volume" = 100;
-          "scroll-step" = 5;
-          on-click = "${pkgs.pavucontrol}/bin/pavucontrol";
-        };
-
-        # my custom modules
-        # "custom/power" = {
-        #   format = "";
-        #   tooltip = false;
-        #   on-click = "exec ${./scripts/power-menu.sh}";
-        # };
-      };
-    };
   };
 
   programs.hyprlock = {
