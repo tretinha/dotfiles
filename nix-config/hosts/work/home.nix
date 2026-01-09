2a218a43b1afa89a0f8d59bbccbb9d3cdeadc802
colombini-gw <gustavo.colombini@glydways.com>
2026-01-09 08:49:54 -0300

    Auto-commit: 2026-01-09 08:49:54

--- nix-config/hosts/work/home.nix
+++ nix-config/hosts/work/home.nix
@@ -17,6 +17,7 @@ in
 {
   imports = [
     ../../modules/home/common.nix
+    ./zen-browser.nix
   ];
 
   targets.genericLinux = {
@@ -49,34 +50,34 @@ in
     slack
   ];
 
-  programs.zen-browser = {
-    enable = true;
-    profiles."default" = {
-      containersForce = true;
-      containers = {
-        "Work" = {
-          color = "green";
-          icon = "briefcase";
-          id = 2;
-        };
-      };
-      spaces = let
-        containers = config.programs.zen-browser.profiles."default".containers;
-      in {
-        "Work" = {
-          id = "61890944-4b85-438a-a4fc-c044f71bc9e7";
-          container = containers."Work".id;
-          position = 2000;
-        };
-      };
-      extensions.packages = [
-        pkgs.firefox-addons.ublock-origin
-        pkgs.firefox-addons.onepassword-password-manager
-        # glean extension had to be manually installed since apparently it's
-        # not available in the nur repo
-      ];
-    };
-  };
+  # programs.zen-browser = {
+  #   enable = true;
+  #   profiles."default" = {
+  #     containersForce = true;
+  #     containers = {
+  #       "Work" = {
+  #         color = "green";
+  #         icon = "briefcase";
+  #         id = 2;
+  #       };
+  #     };
+  #     spaces = let
+  #       containers = config.programs.zen-browser.profiles."default".containers;
+  #     in {
+  #       "Work" = {
+  #         id = "61890944-4b85-438a-a4fc-c044f71bc9e7";
+  #         container = containers."Work".id;
+  #         position = 2000;
+  #       };
+  #     };
+  #     extensions.packages = [
+  #       pkgs.firefox-addons.ublock-origin
+  #       pkgs.firefox-addons.onepassword-password-manager
+  #       # glean extension had to be manually installed since apparently it's
+  #       # not available in the nur repo
+  #     ];
+  #   };
+  # };
 
   programs.waybar = {
     enable = true;
---
+++ nix-config/hosts/work/zen-browser.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.zen-browser = {
+    enable = true;
+    profiles."default" = {
+      containersForce = true;
+      containers = {
+        "Work" = {
+          color = "green";
+          icon = "briefcase";
+          id = 2;
+        };
+      };
+      spaces = let
+        containers = config.programs.zen-browser.profiles."default".containers;
+      in {
+        "Work" = {
+          id = "61890944-4b85-438a-a4fc-c044f71bc9e7";
+          container = containers."Work".id;
+          position = 2000;
+        };
+      };
+      extensions.packages = [
+        pkgs.firefox-addons.ublock-origin
+        pkgs.firefox-addons.onepassword-password-manager
+        # glean extension had to be manually installed since apparently it's
+        # not available in the nur repo
+      ];
+    };
+  };
+}
