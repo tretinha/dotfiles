583e94becd766ce3e206a5de47c8ced7ece44d18
colombini-gw <gustavo.colombini@glydways.com>
2026-03-21 16:00:03 -0300

    Auto-commit: 2026-03-21 16:00:03

--- nix-config/config/nvim/lua/plugins/nvim-treesitter.lua
+++ nix-config/config/nvim/lua/plugins/nvim-treesitter.lua
@@ -4,7 +4,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.config").setup({
+      require("nvim-treesitter.configs").setup({
         ensure_installed = { "c", "lua", "vimdoc", "cue", "terraform", "yaml", "gotmpl", "helm" }, -- Specify parsers to install
         highlight = { enable = false }, -- Not particularly a fan
         indent = { enable = true },
